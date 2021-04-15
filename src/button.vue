9f46cbcae251edb7c00e317d535e535dcf98eb9f
Mcguffen <504177380@qq.com>
2021-04-15 11:50:55 +0800

    完成单文件组件g-button,引入vue完整版来使用parcel。

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export  default {}
+</script>
+
+<style lang="scss">
+.g-button{
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;/*按钮不设置宽度，使用padding左右1em来控制*/
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  /* &来代表当前的选择器 */
+  &:hover{
+    border-color: var(--border-color-hover);
+  }
+  &:active{
+    background-color: var(--button-active-bg);
+  }
+  &:focus{
+    outline: none;
+  }
+}
+
+</style>
