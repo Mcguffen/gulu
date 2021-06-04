91b0c549c407d80c2e5d131ddf04ca1f972bb405
Mcguffen <504177380@qq.com>
2021-06-04 16:59:12 +0800

    优化三种动画。

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ import Input from './input'
 import Row from './row'
 import Col from './col'
 import Footer from './footer'
-import Content from './content'
+import content from './content'
 import Layout from './layout'
 import Header from './header'
 import Sider from './sider'
-import Toast from "./toast"
+import Toast from "./toast";
 import plugin from './plugin'
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -19,7 +19,7 @@ Vue.component('g-input',Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
 Vue.component('g-footer',Footer)
-Vue.component('g-content',Content)
+Vue.component('g-content',content)
 Vue.component('g-layout',Layout)
 Vue.component('g-header',Header)
 Vue.component('g-sider',Sider)
@@ -38,7 +38,15 @@ new Vue({
         inputChange(e){
             console.log(e.target.value)
         },
-        showToast(){
+        showToast1(){
+            this.showToast('top')
+        },
+        showToast2(){
+            this.showToast('middle')
+        },showToast3(){
+            this.showToast('bottom')
+        },
+        showToast(position){
             this.$toast('<strong>你的智商需要充值</strong>',{
                 enableHtml: true,
                 closeButton: {
@@ -49,7 +57,7 @@ new Vue({
                 },
                 autoClose:false,
                 autoCloseDelay: 3,
-                position:'bottom'
+                position:position
             })
         }
     }
