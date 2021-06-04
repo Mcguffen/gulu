9ec1622715758b5ad5116a3f0fd7560d62dd6629
Mcguffen <504177380@qq.com>
2021-06-04 16:56:50 +0800

    解决了currentToast的一个bug，实现进入动画。

--- src/plugin.js
+++ src/plugin.js
@@ -1,4 +1,3 @@
-
 //为什么要用plugin，1.避免吧vue配到另外一个轮子上
 // 2.$toast可能会覆盖现有的东西，但我们不确定，所以让用户自己去覆盖
 import Toast from './toast'
@@ -11,12 +10,18 @@ export default {
             if(currentToast){
                 currentToast.close()
             }
-            currentToast=createToast({Vue, message, propsData: toastOptions})
+            currentToast=createToast({Vue,
+                message,
+                propsData: toastOptions,
+                onClose:()=>{
+                    currentToast=null
+                }
+            })
         }
     }
 }
 //helpers
-let createToast = ({Vue, message, propsData}) => {
+let createToast = ({Vue, message, propsData,onClose}) => {
     let Constructor = Vue.extend(Toast)
     let toast = new Constructor({
         propsData: propsData
@@ -24,6 +29,7 @@ let createToast = ({Vue, message, propsData}) => {
     //slot要放在mount前面
     toast.$slots.default = [message]
     toast.$mount()
+    toast.$on('close',onClose)
     document.body.appendChild(toast.$el)
     return toast
 }
