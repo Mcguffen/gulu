6ea6f2073dd32173a1bb9b5691a5ee80278860ef
Mcguffen <504177380@qq.com>
2021-06-04 15:00:12 +0800

    添加row的align属性

--- src/col.vue
+++ src/col.vue
@@ -1,44 +1,54 @@
 <template>
-  <div class="col" :class="[span&&`col-${span}`,offset && `offset-${offset}`]"
-       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
-    <div style="border: 1px solid green;height: 100px;">
+  <div class="col" :class="colClass"
+       :style="colStyle">
+    <div>
       <slot></slot>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'GuluCol',
-    props:{
-        span:[Number,String],
-        offset: {
-          type: [Number, String]
-        }
-    },
+  name: 'GuluCol',
+  props: {
+    span: [Number, String],
+    offset: {
+      type: [Number, String]
+    }
+  },
   data() {
     return {
       gutter: 0
+    }
+  },
+  computed: {
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
+    },
+    colClass() {
+      return [this.span && `col-${this.span}`, this.offset && `offset-${this.offset}`]
     }
   }
 }
 </script>
 
-<style scoped lang='scss'>
-.col{
-    padding: 0 10px;
-    width: 50%;
-    $class-prefix: col-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        width: ($n/24)*100%;
-      }
+<style scoped lang="scss">
+.col {
+  padding: 0 10px;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24)*100%;
     }
-    $class-prefix: offset-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix}#{$n} {
-        margin-left: ($n/24)*100%;
-      }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24)*100%;
     }
+  }
 }
 </style>
